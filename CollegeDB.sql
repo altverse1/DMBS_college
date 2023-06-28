ba7ad62eadc86302a4e871d546c678fe7421452c
Alt_nant <131745606+altverse1@users.noreply.github.com>
2023-06-28 14:49:33 +0530

    Add files via upload

--- CollegeDB.sql
+++ CollegeDB.sql
@@ -44,84 +44,47 @@ create table iamarks(
 	FOREIGN KEY(subcode) references subject(subcode) on delete cascade,
 	FOREIGN KEY(ssid) references semsec(ssid) on delete cascade
 );
- 
 
--- Insert values for the 'student' table
+
 INSERT INTO student (usn, sname, address, phone, gender)
-VALUES
-    ('1BI15CS101', 'John Doe', '123 Main Street', 1234567890, 'Male'),
-    ('1BI15CS102', 'Jane Smith', '456 Elm Avenue', 9876543210, 'Female'),
-    ('1BI15CS103', 'Mike Johnson', '789 Oak Road', 5555555555, 'Male'),
-    ('1BI15CS104', 'Emily Brown', '321 Pine Court', 1111111111, 'Female'),
-    ('1BI15CS105', 'David Wilson', '654 Cedar Lane', 2222222222, 'Male'),
-    ('1BI15CS106', 'Sarah Davis', '987 Maple Drive', 3333333333, 'Female'),
-    ('1BI15CS107', 'Michael Miller', '741 Birch Street', 4444444444, 'Male'),
-    ('1BI15CS108', 'Jennifer Anderson', '852 Walnut Avenue', 6666666666, 'Female'),
-    ('1BI15CS109', 'Robert Taylor', '963 Spruce Road', 7777777777, 'Male'),
-    ('1BI15CS110', 'Jessica Wilson', '159 Pine Court', 8888888888, 'Female');
+VALUES ('USN001', 'John Doe', '123 Main Street', 1234567890, 'Male'),
+('USN002', 'Jane Smith', '456 Elm Street', 9876543210, 'Female'),
+('USN003', 'Mark Johnson', '789 Oak Street', 5551234567, 'Male'),
+('USN004', 'Emma Williams', '987 Pine Street', 5559876543, 'Female'),
+('1BI15CS101', 'Michael Brown', '321 Elm Street', 5553456789, 'Male'),
+('USN006', 'Olivia Taylor', '654 Cherry Lane', 5558765432, 'Female'),
+('USN005', 'David Smith', '456 Maple Avenue', 5552345678, 'Male');
 
--- Insert values for the 'semsec' table
 INSERT INTO semsec (ssid, sem, sec)
-VALUES
-    ('SSID001', '1', 'A'),
-    ('SSID002', '1', 'B'),
-    ('SSID003', '2', 'A'),
-    ('SSID004', '2', 'B'),
-    ('SSID005', '3', 'A'),
-    ('SSID006', '3', 'B'),
-    ('SSID007', '4', 'A'),
-    ('SSID008', '4', 'B'),
-    ('SSID009', '5', 'A'),
-    ('SSID010', '5', 'B'),
-    ('SSID011', '8', 'A'),
-    ('SSID012', '8', 'B'),
-    ('SSID013', '8', 'C');
-
--- Insert values for the 'class' table
+VALUES ('SSID001', '4', 'A'),
+('SSID002', '3', 'B'),
+('SSID003', '8', 'C'),
+('SSID004', '8', 'A'),
+('SSID005', '8', 'B'),
+('SSID006', '8', 'C');
+ 
+ 
 INSERT INTO class (usn, ssid)
-VALUES
-    ('1BI15CS101', 'SSID011'),
-    ('1BI15CS102', 'SSID011'),
-    ('1BI15CS103', 'SSID011'),
-    ('1BI15CS104', 'SSID011'),
-    ('1BI15CS105', 'SSID011'),
-    ('1BI15CS106', 'SSID011'),
-    ('1BI15CS107', 'SSID011'),
-    ('1BI15CS108', 'SSID011'),
-    ('1BI15CS109', 'SSID012'),
-    ('1BI15CS110', 'SSID013');
-
--- Insert values for the 'subject' table
+VALUES ('USN001', 'SSID001'),
+('USN002', 'SSID001'),
+('USN003', 'SSID002'),
+('USN004', 'SSID002'),
+('1BI15CS101', 'SSID003'),
+('USN005', 'SSID004'),
+('USN006', 'SSID005'),
+('USN006', 'SSID006');
+ 
 INSERT INTO subject (subcode, title, sem, credits)
-VALUES
-    ('SUB001', 'Mathematics', '1', 4),
-    ('SUB002', 'English', '1', 3),
-    ('SUB003', 'Physics', '2', 4),
-    ('SUB004', 'Chemistry', '2', 4),
-    ('SUB005', 'Biology', '3', 4),
-    ('SUB006', 'History', '3', 3),
-    ('SUB007', 'Geography', '4', 3),
-    ('SUB008', 'Computer Science', '4', 4),
-    ('SUB009', 'Economics', '5', 3),
-    ('SUB010', 'Psychology', '5', 3),
-    ('SUB011', 'Artificial Intelligence', '8', 3),
-    ('SUB012', 'Machine Learning', '8', 4),
-    ('SUB013', 'Database Systems', '8', 3);
+VALUES ('SUB001', 'Mathematics', '8', 4),
+('SUB002', 'Physics', '8', 3),
+('SUB003', 'Chemistry', '8', 4),
+('SUB004', 'Biology', '8', 3);
 
--- Insert values for the 'iamarks' table
 INSERT INTO iamarks (usn, subcode, ssid, test1, test2, test3, finalia)
-VALUES
-    ('1BI15CS101', 'SUB001', 'SSID011', 90, 85, 92, 95),
-    ('1BI15CS102', 'SUB001', 'SSID011', 80, 92, 88, 87),
-    ('1BI15CS103', 'SUB001', 'SSID011', 95, 93, 90, 94),
-    ('1BI15CS104', 'SUB001', 'SSID011', 88, 90, 92, 91),
-    ('1BI15CS105', 'SUB001', 'SSID011', 92, 87, 85, 89),
-    ('1BI15CS106', 'SUB001', 'SSID011', 91, 94, 88, 90),
-    ('1BI15CS107', 'SUB001', 'SSID011', 86, 88, 90, 88),
-    ('1BI15CS108', 'SUB001', 'SSID011', 89, 91, 87, 90),
-    ('1BI15CS109', 'SUB001', 'SSID012', 94, 92, 95, 93),
-    ('1BI15CS110', 'SUB001', 'SSID013', 87, 90, 86, 88);
-
+VALUES ('1BI15CS101', 'SUB001', 'SSID003', 80, 75, 85, NULL),
+('1BI15CS101', 'SUB002', 'SSID003', 90, 85, 80, NULL),
+('USN005', 'SUB003', 'SSID004', 70, 75, 80, NULL),
+('USN006', 'SUB004', 'SSID005', 85, 90, 95, NULL);
 
 select * from student; 
 select * from semsec;
@@ -129,6 +92,11 @@ select * from class;
 select * from subject;
 select * from iamarks;
 
+DROP Table student;
+DROP Table semsec;
+DROP Table class;
+DROP Table subject;
+DROP Table iamarks;
 
 -- Q.List all the student details studying in fourth semester 'A' Section
 select s.usn, sname, gender, address
